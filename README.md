71469eca43d9b64983376d56bfcac6e441ba261e
Guillaume L'HERMINIER <guillaumelherminier@MacBook-Pro-de-Guillaume-LHERMINIER.local>
2017-11-26 21:33:26 +0100

    UBERMOOV

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-Symfony Azure Edition
-=====================
+my_project_name
+===============
 
-
-Welcome to the Symfony Azure Edition - a fully-functional Symfony2
-application that you can use as the skeleton for your new applications
-running on Windows Azure Websites or Cloud Services.
-
-You can get this application running on Windows Azure by performing
-the following steps:
-
-1. Create a Windows Azure account
-2. Create a new website using Git deployment and SQL Server database
-3. Create a Storage Account (for Table)
-4. Add the git remote data to this repository, for example:
-
-        git remote add azure <someurl>
-
-5. Follow the documentation on [deploying to Windows Azure Websites](http://beberlei.github.io/AzureDistributionBundle/composer.html)
-
-6. Push the code to Windows Azure.
-
-        git push azure master
-
+A Symfony project created on November 15, 2017, 1:45 pm.
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,46 +3,56 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-use WindowsAzure\DistributionBundle\HttpKernel\AzureKernel;
-use WindowsAzure\DistributionBundle\DependencyInjection\EnvironmentResource;
-
-class AppKernel extends AzureKernel
+class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new WindowsAzure\DistributionBundle\WindowsAzureDistributionBundle(),
-            new WindowsAzure\TaskDemoBundle\WindowsAzureTaskDemoBundle(),
-        );
+            new AppBundle\AppBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+   
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+        }
         }
 
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-
-        $envParameters = $this->getEnvParameters();
-        $loader->load(function($container) use($envParameters) {
-            $container->getParameterBag()->add($envParameters);
-            $container->addResource(new EnvironmentResource($envParameters));
-        });
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
