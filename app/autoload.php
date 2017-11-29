f70f439dd711fa8bcb8f3b08d2243162b7105846
Guillaume L'HERMINIER <guillaumelherminier@MacBook-Pro-de-Guillaume-LHERMINIER.local>
2017-11-29 16:06:15 +0100

    UBERMOOV

--- app/autoload.php
+++ app/autoload.php
@@ -5,6 +5,11 @@ use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+// $kernel->loadClassCache();
+$request = Request::createFromGlobals();
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
